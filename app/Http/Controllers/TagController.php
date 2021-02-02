9eea67b94ce6caef4c34f5cdf9275079eedeef0a
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-02 12:33:09 -0600

    store method for Tagcontroller creates a new tag and returns a redirect

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tag;
 
 class TagController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        Tag::create($request->all());
+
+        return redirect('/');
+    }
 }
