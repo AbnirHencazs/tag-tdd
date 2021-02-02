174de802e68c6d889c2b90f4b14099e4a23e8be4
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-02 12:13:47 -0600

    testing existence of data in welcome view

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -6,11 +6,23 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\Models\Tag;
+
 class HomeTest extends TestCase
 {
     use RefreshDatabase;
     public function testEmpty()
     {
         $this->get('/')->assertStatus(200)->assertSee('No hay etiquetas');
+    }
+
+    public function testDataNotNull()
+    {
+        $tag = Tag::factory()->create();
+        $this
+            ->get('/')
+            ->assertStatus(200)
+            ->assertSee($tag->name)
+            ->assertDontSee('No hay etiquetas');
     }
 }
