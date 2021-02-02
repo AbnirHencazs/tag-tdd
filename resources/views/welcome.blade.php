fb8ae3eaf69d0bf8152ad8514c35c03a14d4ba90
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-02 13:14:31 -0600

    Add forms to create and delete tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,23 @@
     <title>Laravel</title>
 </head>
 <body>
+    <form action="tags" method="post">
+        @csrf
+        <input type="text" name="name" id="">
+        <input type="submit" value="Agregar">
+    </form>
     <h4>Listado de etiquetas</h4>
     <table>
         @forelse( $tags as $tag )
             <tr>
                 <td>{{ $tag->name }}</td>
+                <td>
+                    <form action="tags/{{$tag->id}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Borrar">
+                    </form>
+                </td>
             </tr>
         @empty
             <tr>
